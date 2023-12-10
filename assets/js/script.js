007b3832bd14bc0283db7b14a37c47c5ff163b1c
LeWilSy <b.tighidet0@gmail.com>
2023-12-10 18:16:53 +0100

    RESPONSIVE IMG SRC

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 const header = document.getElementById('header')
-    hero = document.getElementById('hero');
+    hero = document.getElementById('hero')
+    logoFooter = document.getElementById('logo-footer');
+
+
 var headerHeight = header.offsetHeight
     heroHeight = hero.offsetHeight;
     beforeHeight = heroHeight + headerHeight + 20 +"px";
 
-document.documentElement.style.setProperty('--beforeHeight', beforeHeight)
+document.documentElement.style.setProperty('--beforeHeight', beforeHeight);
+
+
+// change file source of image depending on width device
+
+if( window.innerWidth <= 600) {
+    logoFooter.src = './médias/logo-musée-d_aquitaine-footer-mobile.webp'
+} else {
+    logoFooter.src = './médias/logo musée d_aquitaine footer.webp'
+  }
