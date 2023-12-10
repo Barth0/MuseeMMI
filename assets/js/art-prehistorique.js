6b3a49ab26f521bfddd789e04576cdd5bf606449
LeWilSy <b.tighidet0@gmail.com>
2023-12-10 15:05:46 +0100

    Merge branch 'main' of https://github.com/Barth0/MuseeMMI

--- assets/js/art-prehistorique.js
+++ assets/js/art-prehistorique.js
@@ -1,9 +1,9 @@
-const playerIframe = document.querySelector('.player-iframe'),
-      iframe = playerIframe.querySelector('iframe'),
-      unloadedSrc = iframe.getAttribute('data-src'),
-      image = playerIframe.querySelector('img');
-playerIframe.addEventListener('click', () => {
-  iframe.setAttribute('src', unloadedSrc);
+const playerIframe = document.querySelector(".player-iframe"),
+  iframe = playerIframe.querySelector("iframe"),
+  unloadedSrc = iframe.getAttribute("data-src"),
+  image = playerIframe.querySelector("img");
+playerIframe.addEventListener("click", () => {
+  iframe.setAttribute("src", unloadedSrc);
   image.style.display = "none";
   iframe.style.display = "block";
 });
